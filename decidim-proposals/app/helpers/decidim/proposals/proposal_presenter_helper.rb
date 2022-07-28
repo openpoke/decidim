fe64ec17019e3c252251f6c7e339b3eba683e1ec
Ivan Vergés <ivan@pokecode.net>
2022-07-28 16:42:02 +0200

    fix methods in helper

--- decidim-proposals/app/helpers/decidim/proposals/proposal_presenter_helper.rb
+++ decidim-proposals/app/helpers/decidim/proposals/proposal_presenter_helper.rb
@@ -14,24 +14,14 @@ module Decidim
       # If the proposal is official or the rich text editor is enabled on the
       # frontend, the proposal body is considered as safe content; that's unless
       # the proposal comes from a collaborative_draft or a participatory_text.
-      def safe_content?(proposal)
-        proposal.organization.rich_text_editor_in_public_views? && not_from_collaborative_draft(proposal) ||
-          (proposal.official? || proposal.official_meeting?) && not_from_participatory_text(proposal)
-      end
-
-      def render_proposal_title(proposal)
-        Decidim::Proposals::ProposalPresenter.new(proposal).title(links: true, html_escape: true)
+      def safe_content?
+        rich_text_editor_in_public_views? && not_from_collaborative_draft(@proposal) ||
+          (@proposal.official? || @proposal.official_meeting?) && not_from_participatory_text(@proposal)
       end
 
       # If the content is safe, HTML tags are sanitized, otherwise, they are stripped.
       def render_proposal_body(proposal)
-        safe = safe_content?(proposal)
-        body = Decidim::Proposals::ProposalPresenter.new(proposal).body(links: true, strip_tags: !safe)
-        body = simple_format(body, {}, sanitize: false)
-
-        return body unless safe
-
-        decidim_sanitize(body)
+        Decidim::ContentProcessor.render(render_sanitized_content(proposal, :body), "div")
       end
     end
   end
