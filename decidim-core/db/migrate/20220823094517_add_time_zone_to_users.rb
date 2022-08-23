22547f48de7622804b10e15b10735a70cf3a0dcf
antopalidi <t.opalidisanna@gmail.com>
2022-08-23 21:13:11 +0200

    add tests: time_zone in account_form

--- decidim-core/db/migrate/20220823094517_add_time_zone_to_users.rb
+++ decidim-core/db/migrate/20220823094517_add_time_zone_to_users.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddTimeZoneToUsers < ActiveRecord::Migration[6.0]
   def change
     add_column :decidim_users, :time_zone, :string, limit: 255, default: "UTC"
