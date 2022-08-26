cf41ee71a53fac9cb788f0be1e51874568231670
Ivan Vergés <ivan@pokecode.net>
2022-08-26 22:37:44 +0200

    Diff booster (#13)
    * update ux for diff
    
    * force update
    
    * prevent arrays in diff
    
    * fix methods in helper
    
    * restore deleted method in helper
    
    * fix errors
    
    * Update diff_cell.rb
    
    * use dig to avoid nil errors

--- decidim-proposals/app/services/decidim/proposals/diff_renderer.rb
+++ decidim-proposals/app/services/decidim/proposals/diff_renderer.rb
@@ -3,6 +3,19 @@
 module Decidim
   module Proposals
     class DiffRenderer < BaseDiffRenderer
+      include ActionView::Helpers::TextHelper
+      include ActionView::Helpers::TagHelper
+      include ProposalPresenterHelper
+      include SanitizeHelper
+
+      delegate :organization, to: :proposal, prefix: :current
+
+      def preview
+        title = content_tag(:h3, render_proposal_title(proposal), class: "heading3")
+        body = content_tag(:div, render_proposal_body(proposal), class: "body")
+        content_tag(:div, "#{title}#{body}".html_safe, class: "diff-preview diff-proposal")
+      end
+
       private
 
       # Lists which attributes will be diffable and how they should be rendered.
