5401b1f16422c37cabfc20cbbfcdb76df13bc2a6
antopalidi <t.opalidisanna@gmail.com>
2022-08-26 10:00:09 +0200

    change default timezone

--- decidim-core/db/migrate/20220823094517_add_time_zone_to_users.rb
+++ decidim-core/db/migrate/20220823094517_add_time_zone_to_users.rb
@@ -2,6 +2,6 @@
 
 class AddTimeZoneToUsers < ActiveRecord::Migration[6.0]
   def change
-    add_column :decidim_users, :time_zone, :string, limit: 255, default: "UTC"
+    add_column :decidim_users, :time_zone, :string
   end
 end
