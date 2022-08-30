e9651ca11807a79b9016919a3ae57d8ee677096e
antopalidi <t.opalidisanna@gmail.com>
2022-08-30 21:33:35 +0200

    fix proposals helper

--- decidim-proposals/app/helpers/decidim/proposals/admin/proposals_helper.rb
+++ decidim-proposals/app/helpers/decidim/proposals/admin/proposals_helper.rb
@@ -150,7 +150,8 @@ module Decidim
         def evaluating_proposals_overdue?(proposal)
           grace_period = Decidim::Proposals.config.evaluating_proposals_overdue.to_i
           !grace_period.zero? &&
-            proposal.evaluating? && (Time.current - grace_period.days).to_date > proposal.answered_at
+            proposal.evaluating? && proposal.answered? &&
+            (Time.current - grace_period.days).to_date > proposal.answered_at
         end
 
         def grace_period_unanswered?(proposal)
@@ -168,7 +169,7 @@ module Decidim
 
         def last_day_to_evaluate(proposal)
           grace_period = Decidim::Proposals.config.evaluating_proposals_overdue.to_i
-          (proposal.answered_at + grace_period.days).to_date if proposal.answered_at
+          (proposal.answered_at + grace_period.days).to_date if proposal.answered?
         end
       end
     end
