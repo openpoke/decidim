b459da8a224c9516a0cdfce3471fee02d5dc9e91
Ivan Vergés <ivan@pokecode.net>
2022-09-19 10:36:25 +0200

    Send email to the author when a content is hidden (#30)
    * send email to author when resource was hidden
    
    * fix: authors
    
    * fix: i18n normalize
    
    * fix: remove extra code, change hide_resource
    
    * add tests
    
    * fix test and change email view
    
    * remove extra lines and spans
    
    * fix test: change a variable name
    
    Co-authored-by: antopalidi <t.opalidisanna@gmail.com>

--- decidim-admin/app/commands/decidim/admin/hide_resource.rb
+++ decidim-admin/app/commands/decidim/admin/hide_resource.rb
@@ -26,6 +26,8 @@ module Decidim
 
         send_hide_notification_to_author
 
+        send_hide_email_to_author
+
         broadcast(:ok, @reportable)
       end
 
@@ -61,6 +63,16 @@ module Decidim
         }
 
         Decidim::EventsManager.publish(data)
+      end
+
+      def send_hide_email_to_author
+        Decidim::Admin::HiddenResourceMailer.notify_mail(
+          @reportable, resource_authors, report_reasons
+        ).deliver_later
+      end
+
+      def resource_authors
+        @reportable.try(:authors) || [@reportable.try(:author)]
       end
 
       def report_reasons
