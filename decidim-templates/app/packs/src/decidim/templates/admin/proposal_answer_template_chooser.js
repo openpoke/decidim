904dc0e468998821af6da9a5643fda5fcfff6210
Alexandru-Emil Lupu <contact@alecslupu.ro>
2022-10-11 19:56:21 +0300

    Running EsLint

--- decidim-templates/app/packs/src/decidim/templates/admin/proposal_answer_template_chooser.js
+++ decidim-templates/app/packs/src/decidim/templates/admin/proposal_answer_template_chooser.js
@@ -1,19 +1,21 @@
 $(() => {
-  $("#proposal_answer_template_chooser").change(function(){
-      $.getJSON($(this).data('url'), {
-        id: $(this).val(),
-        proposal_id: $(this).data('proposal')
-      }).done(function(data){
-        $("#proposal_answer_internal_state_" + data.state).trigger('click');
+  $("#proposal_answer_template_chooser").change(function() {
+    let dropDown =  $("#proposal_answer_template_chooser");
+    $.getJSON(dropDown.data("url"), {
+      id: dropDown.val(),
+      /* eslint camelcase: [0] */
+      proposal_id: dropDown.data("proposal")
+    }).done(function(data) {
+      $(`#proposal_answer_internal_state_${data.state}`).trigger("click");
 
-        let $editors = $("#proposal_answer_template_chooser").parent().parent().find('.tabs-panel').find('.editor-container');
-        $editors.each(function(index, element){
-          local_element = $(element);
-          let $locale = local_element.siblings('input[type=hidden]').attr('id').replace('proposal_answer_answer_', '');
-          let editor = Quill.find(element);
-          let delta = editor.clipboard.convert(data.template[$locale]);
-          editor.setContents(delta, 'silent');
-        });
+      let $editors = dropDown.parent().parent().find(".tabs-panel").find(".editor-container");
+      $editors.each(function(index, element) {
+        let localElement = $(element);
+        let $locale = localElement.siblings("input[type=hidden]").attr("id").replace("proposal_answer_answer_", "");
+        let editor = Quill.find(element);
+        let delta = editor.clipboard.convert(data.template[$locale]);
+        editor.setContents(delta, "silent");
       });
     });
+  });
 });
