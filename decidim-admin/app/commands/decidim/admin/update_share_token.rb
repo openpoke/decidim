8aa02c691fb79bad64269b1ce179da8f3e851184
Anna Topalidi <t.opalidisanna@gmail.com>
2024-09-05 14:22:08 +0200

    fix UpdateShareToken

--- decidim-admin/app/commands/decidim/admin/update_share_token.rb
+++ decidim-admin/app/commands/decidim/admin/update_share_token.rb
@@ -2,10 +2,47 @@
 
 module Decidim
   module Admin
-    # A command with all the business logic to update a taxonomy.
+    # A command with all the business logic to update a share token.
     # This command is called from the controller.
-    class UpdateShareToken < Decidim::Commands::UpdateResource
-      fetch_form_attributes :expires_at, :registered_only
+    class UpdateShareToken < Decidim::Command
+      # Public: Initializes the command.
+      #
+      # form - A form object with the params.
+      # token - The ShareToken object to update.
+      def initialize(form, token)
+        @form = form
+        @token = token
+      end
+
+      # Executes the command. Broadcasts these events:
+      #
+      # - :ok when everything is valid.
+      # - :invalid if the form wasn't valid and we couldn't proceed.
+      #
+      # Returns nothing.
+      def call
+        return broadcast(:invalid) if form.invalid?
+
+        update_share_token
+        broadcast(:ok)
+      end
+
+      private
+
+      attr_reader :form, :token
+
+      def update_share_token
+        Decidim.traceability.update!(
+          token,
+          form.user,
+          {
+            expires_at: form.expires_at,
+            token_for: form.token_for,
+            organization: form.organization,
+            user: form.user
+          }
+        )
+      end
     end
   end
 end
