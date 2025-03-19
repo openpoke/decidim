e438fd887a093b111584dfbf74bd509f2d9379bd
Anna Topalidi <t.opalidisanna@gmail.com>
2025-03-19 12:06:17 +0100

    fix reminder error

--- decidim-meetings/app/commands/decidim/meetings/create_meeting.rb
+++ decidim-meetings/app/commands/decidim/meetings/create_meeting.rb
@@ -79,9 +79,10 @@ module Decidim
         return if meeting.start_time < Time.zone.now
 
         checksum = Decidim::Meetings::UpcomingMeetingNotificationJob.generate_checksum(meeting)
+        reminder_hours = (meeting.send_reminders_before_hours.presence || Decidim::Meetings.upcoming_meeting_notification.in_hours).to_i
 
         Decidim::Meetings::UpcomingMeetingNotificationJob
-          .set(wait_until: meeting.start_time - form.send_reminders_before_hours.hours)
+          .set(wait_until: meeting.start_time - reminder_hours.hours)
           .perform_later(meeting.id, checksum)
       end
 
